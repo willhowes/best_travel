b730a636f155ee36fad66bd9cb4c118fb09e1644
eldenhowes <williameldenhowes@gmail.com>
2019-05-04 14:15:54 +0100

    first commit

---
+++ best_travel.rb
@@ -0,0 +1,13 @@
+def choose_best_sum(t, k, ls)
+    # your code
+end
+
+puts "---TEST 1----"
+choose_best_sum(163, 3, [50, 55, 56, 57, 58])
+#shoudl return 163
+puts "---TEST 2----"
+choose_best_sum(163, 3, [50])
+#should return nil
+puts "---TEST 3----"
+choose_best_sum(230, 3, [91, 74, 73, 85, 73, 81, 87])
+#should return 228
